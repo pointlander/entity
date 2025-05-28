a2b9fb01bcedda9bccad79c02baad4a3e01f4674
Andrew Snodgrass <pointlander@gmail.com>
2025-05-27 20:04:01 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,255 @@
+// Copyright 2024 The Entity Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"embed"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+	"strconv"
+	"strings"
+
+	"github.com/pointlander/gradient/tf32"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+)
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = 1.0e-3
+	// Scale is the scale of the model
+	Scale = 128
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
+)
+
+//go:embed iris.zip
+var Iris embed.FS
+
+// Fisher is the fisher iris data
+type Fisher struct {
+	Measures []float64
+	Label    string
+	Cluster  int
+	Index    int
+}
+
+// Labels maps iris labels to ints
+var Labels = map[string]int{
+	"Iris-setosa":     0,
+	"Iris-versicolor": 1,
+	"Iris-virginica":  2,
+}
+
+// Load loads the iris data set
+func Load() []Fisher {
+	file, err := Iris.Open("iris.zip")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	fisher := make([]Fisher, 0, 8)
+	reader, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range reader.File {
+		if f.Name == "iris.data" {
+			iris, err := f.Open()
+			if err != nil {
+				panic(err)
+			}
+			reader := csv.NewReader(iris)
+			data, err := reader.ReadAll()
+			if err != nil {
+				panic(err)
+			}
+			for i, item := range data {
+				record := Fisher{
+					Measures: make([]float64, 4),
+					Label:    item[4],
+					Index:    i,
+				}
+				for i := range item[:4] {
+					f, err := strconv.ParseFloat(item[i], 64)
+					if err != nil {
+						panic(err)
+					}
+					record.Measures[i] = f
+				}
+				fisher = append(fisher, record)
+			}
+			iris.Close()
+		}
+	}
+	return fisher
+}
+
+func main() {
+	iris := Load()
+	avg := make([]float64, 7)
+	for _, v := range iris {
+		labels := make([]float64, 3)
+		labels[Labels[v.Label]] = 1
+		measures := append(v.Measures, labels...)
+		for i, v := range measures {
+			avg[i] += v
+		}
+	}
+	for i := range avg {
+		avg[i] /= float64(len(iris))
+	}
+	cov := [7][7]float64{}
+	for _, v := range iris {
+		labels := make([]float64, 3)
+		labels[Labels[v.Label]] = 1
+		measures := append(v.Measures, labels...)
+		for i, v := range measures {
+			for ii, vv := range measures {
+				diff1 := avg[i] - v
+				diff2 := avg[ii] - vv
+				cov[i][ii] += diff1 * diff2
+			}
+		}
+	}
+	for i := range cov {
+		for j := range cov[i] {
+			cov[i][j] = cov[i][j] / float64(len(iris))
+		}
+	}
+	for i := range cov {
+		fmt.Println(cov[i])
+	}
+	fmt.Println(avg)
+
+	rng := rand.New(rand.NewSource(1))
+
+	set := tf32.NewSet()
+	set.Add("A", 7, 7)
+
+	for i := range set.Weights {
+		w := set.Weights[i]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float32, StateTotal)
+			for i := range w.States {
+				w.States[i] = make([]float32, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for i := 0; i < cap(w.X); i++ {
+			w.X = append(w.X, float32(rng.NormFloat64()*factor))
+		}
+		w.States = make([][]float32, StateTotal)
+		for i := range w.States {
+			w.States[i] = make([]float32, len(w.X))
+		}
+	}
+
+	others := tf32.NewSet()
+	others.Add("E", 7, 7)
+	E := others.ByName["E"]
+	for i := range cov {
+		for j := range cov[i] {
+			E.X = append(E.X, float32(cov[i][j]))
+		}
+	}
+
+	loss := tf32.Sum(tf32.Quadratic(others.Get("E"), tf32.Mul(set.Get("A"), set.Get("A"))))
+
+	points := make(plotter.XYs, 0, 8)
+	for i := 0; i < 1024; i++ {
+		pow := func(x float32) float32 {
+			y := math.Pow(float64(x), float64(i+1))
+			if math.IsNaN(y) || math.IsInf(y, 0) {
+				return 0
+			}
+			return float32(y)
+		}
+
+		set.Zero()
+		others.Zero()
+		cost := tf32.Gradient(loss).X[0]
+		if math.IsNaN(float64(cost)) || math.IsInf(float64(cost), 0) {
+			fmt.Println(i, cost)
+			break
+		}
+
+		norm := float32(0.0)
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = float32(math.Sqrt(float64(norm)))
+		b1, b2 := pow(B1), pow(B2)
+		scaling := float32(1.0)
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+		for _, w := range set.Weights {
+			for l, d := range w.D {
+				g := d * scaling
+				m := B1*w.States[StateM][l] + (1-B1)*g
+				v := B2*w.States[StateV][l] + (1-B2)*g*g
+				w.States[StateM][l] = m
+				w.States[StateV][l] = v
+				mhat := m / (1 - b1)
+				vhat := v / (1 - b2)
+				if vhat < 0 {
+					vhat = 0
+				}
+				w.X[l] -= Eta * mhat / (float32(math.Sqrt(float64(vhat))) + 1e-8)
+			}
+		}
+		points = append(points, plotter.XY{X: float64(i), Y: float64(cost)})
+		fmt.Println(i, cost)
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "epochs.png")
+	if err != nil {
+		panic(err)
+	}
+}
