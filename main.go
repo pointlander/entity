d16f1b217c8725ec8161295132ba740e68cf7ad2
Andrew Snodgrass <pointlander@gmail.com>
2025-07-20 19:37:12 -0500

    Added 8 queens problem

--- main.go
+++ main.go
@@ -351,4 +351,206 @@ func main() {
 		Factor()
 		return
 	}
+
+	rng := rand.New(rand.NewSource(1))
+	fitness := func(board []float32) int {
+		fitness := 0
+		for i := range 8 {
+			for ii := range 8 {
+				for iii := ii + 1; iii < 8; iii++ {
+					if board[i*8+iii] > 0 {
+						fitness++
+						break
+					}
+				}
+				for iii := ii - 1; iii >= 0; iii-- {
+					if board[i*8+iii] > 0 {
+						fitness++
+						break
+					}
+				}
+				for iii := i + 1; iii < 8; iii++ {
+					if board[iii*8+ii] > 0 {
+						fitness++
+						break
+					}
+				}
+				for iii := i - 1; iii >= 0; iii-- {
+					if board[iii*8+ii] > 0 {
+						fitness++
+						break
+					}
+				}
+				x, y := ii, i
+				for {
+					x++
+					y++
+					if x > 7 || y > 7 {
+						break
+					}
+					if board[y*8+x] > 0 {
+						fitness++
+						break
+					}
+				}
+				x, y = ii, i
+				for {
+					x++
+					y--
+					if x > 7 || y < 0 {
+						break
+					}
+					if board[y*8+x] > 0 {
+						fitness++
+						break
+					}
+				}
+				x, y = ii, i
+				for {
+					x--
+					y++
+					if x < 0 || y > 7 {
+						break
+					}
+					if board[y*8+x] > 0 {
+						fitness++
+						break
+					}
+				}
+				x, y = ii, i
+				for {
+					x--
+					y--
+					if x < 0 || y < 0 {
+						break
+					}
+					if board[y*8+x] > 0 {
+						fitness++
+						break
+					}
+				}
+
+			}
+		}
+		return fitness
+	}
+	board := make([]float32, 8*8)
+	for i := range board {
+		board[i] = float32(rng.NormFloat64())
+	}
+	fmt.Println(fitness(board))
+
+	type Number struct {
+		Number  Matrix[float32]
+		Fitness float64
+	}
+
+	const (
+		width      = 64
+		models     = width / 64
+		iterations = 1024
+		population = 1024
+	)
+
+	state := make([][]float32, 256)
+	for i := range state {
+		for range width {
+			state[i] = append(state[i], float32(rng.NormFloat64()))
+		}
+	}
+	pop := make([]Number, population)
+
+	for i := 0; i < iterations; i++ {
+		graph := false //i == 0 || i == iterations-1
+		translate := make([]int, width)
+		for i := range translate {
+			translate[i] = i % models
+		}
+		rng.Shuffle(width, func(i, j int) {
+			translate[i], translate[j] = translate[j], translate[i]
+		})
+		var a, u [models]Matrix[float32]
+		done := make(chan bool, 8)
+		process := func(ii int, seed int64) {
+			rng := rand.New(rand.NewSource(seed))
+			s := make([][]float32, len(state))
+			for iii := range state {
+				for iv, t := range translate {
+					if t == ii {
+						s[iii] = append(s[iii], state[iii][iv])
+					}
+				}
+			}
+			a[ii], _, u[ii] = NewMultiVariateGaussian(.0001, 1.0e-1, graph, false, rng, fmt.Sprintf("number_%d", i), 64, s)
+			done <- true
+		}
+		ii, flight, cpus := 0, 0, runtime.NumCPU()
+		for ii < models && flight < cpus {
+			go process(ii, rng.Int63())
+			flight++
+			ii++
+		}
+		for ii < models {
+			<-done
+			flight--
+
+			go process(ii, rng.Int63())
+			flight++
+			ii++
+		}
+		for range flight {
+			<-done
+		}
+
+		born := pop
+		if i > 0 {
+			born = pop[8:]
+		}
+		learn := func(ii int, seed int64) {
+			rng := rand.New(rand.NewSource(seed))
+			vector := NewMatrix[float32](width, 1)
+			vector.Data = make([]float32, width)
+			for iii := range a {
+				g := NewMatrix[float32](a[iii].Cols, 1)
+				for range a[iii].Cols {
+					g.Data = append(g.Data, float32(rng.NormFloat64()))
+				}
+				vec, index := a[iii].MulT(g).Add(u[iii]), 0
+				for iv, t := range translate {
+					if t == iii {
+						vector.Data[iv] = vec.Data[index]
+						index++
+					}
+				}
+			}
+			born[ii].Number = NewMatrix(width, 1, vector.Data...)
+			born[ii].Fitness = float64(fitness(born[ii].Number.Data))
+			done <- true
+		}
+		ii, flight, cpus = 0, 0, runtime.NumCPU()
+		for ii < len(born) && flight < cpus {
+			go learn(ii, rng.Int63())
+			flight++
+			ii++
+		}
+		for ii < len(born) {
+			<-done
+			flight--
+
+			go learn(ii, rng.Int63())
+			flight++
+			ii++
+		}
+		for range flight {
+			<-done
+		}
+
+		sort.Slice(pop, func(i, j int) bool {
+			return pop[i].Fitness < pop[j].Fitness
+		})
+		for ii := range state {
+			copy(state[ii], pop[ii].Number.Data)
+		}
+		fmt.Println(pop[0].Fitness)
+	}
 }
